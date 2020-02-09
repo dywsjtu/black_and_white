827decf06d4a571b9a1ca51de747d158100ccb68
Yinwei_Dai <dywsjtuji@gmail.com>
2020-02-09 17:57:07 -0500

    add collector color change

---
+++ black_and_white/Assets/Scripts/SmoothFollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowPlayer : MonoBehaviour
+{
+    public GameObject objectToFollow;
+
+    public float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float interpolation = speed * Time.deltaTime;
+        // Vector3 position = this.transform.position;
+        // position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation);
+        // position.x = objectToFollow.transform.position.x;
+        // this.transform.position = position;
+    }
+}
---
+++ black_and_white/Assets/Scripts/StarColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarColor : MonoBehaviour
+{
+     // Start is called before the first frame update
+
+    public Sprite black;
+    public Sprite white;
+
+    Subscription<ChangeColorEvent> color_event_subscription; 
+    void Start()
+    {
+        color_event_subscription = EventBus.Subscribe<ChangeColorEvent>(_OnColorUpdated);
+    }
+
+    // Update is called once per frame
+    void _OnColorUpdated(ChangeColorEvent e)
+    {
+        if (e.white2Black) {
+            GetComponent<SpriteRenderer>().sprite = black;
+        }
+        else 
+        {
+            GetComponent<SpriteRenderer>().sprite = white;
+        }
+    }
+}
