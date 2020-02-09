5cce39a3ddc943c0137f6d42b8406c3c8f7fdeb9
Yinwei_Dai <dywsjtuji@gmail.com>
2020-02-08 21:30:44 -0500

    player movement

---
+++ black_and_white/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed;
+
+    public float jumpHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (moveHorizontal != 0f)
+        {
+            rb.velocity = new Vector2(moveHorizontal*moveSpeed, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rb.velocity.y == 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            }
+        }
+    }
+}
